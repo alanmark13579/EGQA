05dbb6618f0bec17cbb690628b0edfebe8a9d33a
alanmark13579 <korosu1128@gmail.com>
2021-03-01 14:58:09 +0800

    add STT

--- lite/examples/text_classification/android/app/src/main/java/org/tensorflow/lite/examples/textclassification/EGQA/MainActivity.java
+++ lite/examples/text_classification/android/app/src/main/java/org/tensorflow/lite/examples/textclassification/EGQA/MainActivity.java
@@ -16,17 +16,21 @@
 
 package org.tensorflow.lite.examples.textclassification.EGQA;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -50,7 +54,8 @@ public class MainActivity extends AppCompatActivity {
     private Handler handler;
     private ScrollView scrollView;
     private ActivityBinding binding;
-
+    private int SPEECH_REQUEST_CODE = 0;
+    private String STT = "123";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,8 +72,10 @@ public class MainActivity extends AppCompatActivity {
         Button classifyButton = binding.button;
         classifyButton.setOnClickListener(
                 (View v) -> {
-                    classify(inputEditText.getText().toString());
+                     displaySpeechRecognizer();
+
                 });
+
         resultTextView = binding.resultTextView;
         inputEditText = binding.inputText;
         scrollView = binding.scrollView;
@@ -141,5 +148,26 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return Label[FindMood];
+    }
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            Toast.makeText(this,spokenText,Toast.LENGTH_SHORT).show();
+            
+            classify( spokenText);
+        }
     }
 }
